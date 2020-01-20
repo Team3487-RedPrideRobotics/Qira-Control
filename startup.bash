fcae2cdd68e5fad016b419dd2edc72b873be4ac5
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-01-20 12:55:03 -0500

    No need for unecessary wait time

--- startup.bash
+++ startup.bash
@@ -5,5 +5,4 @@ amixer cset numid=3 1
 hook=`cat ../qira-hook.txt`
 sed -i "s@rephook@${hook}@g" docker-compose.yml
 docker-compose up -d
-sleep 25
-chromium-browser --no-user-action --kiosk qira.local/qira-face
+chromium-browser --kiosk qira.local/qira-face
