e08644cf454c6e1d4142c6e19fe2a878d79770e2
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-01-20 11:26:01 -0500

    Replaced Delimiter Char

--- startup.bash
+++ startup.bash
@@ -2,7 +2,7 @@ git reset --hard
 git fetch
 git pull
 hook=`cat ../qira-hook.txt`
-sed -i "s/rephook/${hook}/g" docker-compose.yml
+sed -i "s@rephook@${hook}@g" docker-compose.yml
 docker-compose up -d
 sleep 25
 chromium-browser --no-user-action --kiosk
