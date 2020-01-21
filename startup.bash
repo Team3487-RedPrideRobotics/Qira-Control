b9c0126f326e7cf8a909ef80584fed0e1596a251
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-01-21 08:00:25 -0500

    Added Delay to Chromium

--- startup.bash
+++ startup.bash
@@ -4,6 +4,7 @@ git pull
 amixer cset numid=3 1
 hook=`cat ../qira-hook.txt`
 sed -i "s@rephook@${hook}@g" docker-compose.yml
-docker-compose up -d
-chromium-browser --kiosk --app qira.local/qira-face
 xdotool mousemove 300 300
+docker-compose up -d
+sleep 5
+chromium-browser --kiosk --app qira.local/qira-face
