2b36301cc1847e4660d265488d50a89c87b0353e
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-02-06 09:54:12 -0500

    Added comments

--- startup.bash
+++ startup.bash
@@ -1,10 +1,10 @@
 git reset --hard
 git fetch
 git pull
-amixer cset numid=3 1
+amixer cset numid=3 1 #Set output to 3.5mm
 hook=`cat ../qira-hook.txt`
 sed -i "s@rephook@${hook}@g" docker-compose.yml
 xdotool mousemove 300 300
-docker-compose up -d
+docker-compose up -d #start servers
 sleep 5
-chromium-browser --kiosk --app qira.local/qira-face
+chromium-browser --kiosk --app qira.local/qira-face #start chrome
